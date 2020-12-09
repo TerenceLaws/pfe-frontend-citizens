7d99eddc4e014247f4db0e1d4722623a4c410599
Terence Lawson <terence.lawson@student.vinci.be>
2020-12-09 19:22:56 +0100

    Bugfix erreur 500

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <notifications group="foo" />
+
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+
+import router from "./router/index.js";
+import startScript from "./notification.js"
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+    }
+  },
+  methods:{
+     startScript
+  },
+
+
+  mounted() {
+    console.log("mounted")
+   if (!localStorage.id) {
+     //axios.get("https://pfe-backend-dev.herokuapp.com/notifiaction/publickey")
+     //    .then( r =>{
+     //      console.log(r)
+     //      localStorage.publicKey= r.data.valueOf()  
+     //    })
+     //   .catch(r => console.error(r))
+
+      router.push('getstarted')
+      
+    }
+    this.startScript()
+  }
+
+}
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
